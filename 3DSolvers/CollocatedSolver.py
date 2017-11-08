Fixed issue with derivatives calculations, passed moving wall simulation test in y direction

3DSolvers/CollocatedSolver.py
@@ -71,7 +71,7 @@ class CSolver3D:
         
         self.done = False
         
-        self.Uwall = 0.1
+        self.Uwall = 0.25
         
         #grid info
         self.Nx = domain.Nx
@@ -299,14 +299,17 @@ class CSolver3D:
             self.rhoU1[:,:,-1]  = 0.0
             self.rhoV1[:,:,-1]  = 0.0   
             self.rhoW1[:,:,-1]  = 0.0  
-            
-#            
-#        if self.bcY1 == "ADIABATIC_MOVINGWALL":
-#            self.T[:,-1]  = self.derivY.calcNeumannEnd(self.T)
-#            self.U[:,-1]  = self.Uwall
-#            self.rhoU1[:,-1]  = self.rho1[:,-1]*self.Uwall
-#            self.V[:,-1]  = 0.0
-#            self.rhoV1[:,-1]  = 0.0 
+                        
+        if self.bcY1 == "ADIABATIC_MOVINGWALL":
+            for ip in range(self.Nx):
+                for kp in range(self.Nz):
+                    self.T[ip,-1,kp]  = self.derivY.calcNeumannEnd(self.T[ip,:,kp])
+            self.U[:,-1,:]  = self.Uwall
+            self.V[:,-1,:]  = 0.0
+            self.W[:,-1,:]  = 0.0
+            self.rhoU1[:,-1,:]  = self.rho1[:,-1,:]*self.Uwall
+            self.rhoV1[:,-1,:]  = 0.0 
+            self.rhoW1[:,-1,:]  = 0.0 
 
         if (   self.bcX0 == "ADIABATIC_WALL" 
             or self.bcX1 == "ADIABATIC_WALL" 
@@ -432,14 +435,18 @@ class CSolver3D:
             self.p[:,:,-1]  = self.idealGas.solvePIdealGas(rho[:,:,-1],self.T[:,:,-1])
  
  
-#        if self.bcY1 == "ADIABATIC_MOVINGWALL":
-#            self.U[:,-1]  = self.Uwall
-#            rhoU[:,-1]    = rho[:,-1]*self.Uwall
-#            self.V[:,-1]  = 0.0
-#            rhoV[:,-1]    = 0.0
-#            self.T[:,-1]  = self.derivY.calcNeumannEnd(self.T[:,:])
-#            self.p[:,-1]  = self.idealGas.solvePIdealGas(rho[:,-1],self.T[:,-1])
-#    
+        if self.bcY1 == "ADIABATIC_MOVINGWALL":
+            for ip in range(self.Nx):
+                for kp in range(self.Nz):
+                    self.T[ip,-1,kp]  = self.derivY.calcNeumannEnd(self.T[ip,:,kp])
+            self.U[:,-1,:]  = self.Uwall
+            self.V[:,-1,:]  = 0.0
+            self.W[:,-1,:]  = 0.0
+            self.rhoU1[:,-1,:]  = self.rho1[:,-1,:]*self.Uwall
+            self.rhoV1[:,-1,:]  = 0.0 
+            self.rhoW1[:,-1,:]  = 0.0 
+            self.p[:,-1,:]  = self.idealGas.solvePIdealGas(rho[:,-1,:],self.T[:,-1,:])
+    
     
     def preStepDerivatives(self):
         
@@ -451,7 +458,7 @@ class CSolver3D:
         self.Uy = self.derivY.df_3D(self.U)
         self.Vy = self.derivY.df_3D(self.V)
         self.Wy = self.derivY.df_3D(self.W)
-        
+
         self.Uz = self.derivZ.df_3D(self.U)
         self.Vz = self.derivZ.df_3D(self.V)
         self.Wz = self.derivZ.df_3D(self.W)       
@@ -845,7 +852,7 @@ class CSolver3D:
     def filterConservedValues(self):
         if(self.timeStep%self.filterStep == 0):
             self.numberOfFilterStep += 1
-            
+
             #Need to flip the order of the filtering every other time
             if self.numberOfFilterStep%3 == 1:
                                 
@@ -1008,10 +1015,10 @@ class CSolver3D:
               
     
     def plotFigure(self):
-#        plt.plot(self.x,self.rho1[:,0,0])
+#        plt.plot(self.y,self.U[0,:,0],self.y,self.Uy[0,:,0])
 #        plt.imshow(np.rot90(self.Vx[1:-2,1:-2] - self.Uy[1:-2,1:-2]), cmap="RdBu",interpolation='bicubic')
 #        plt.imshow(np.rot90(self.Ux[1:-2,1:-2] + self.Ux[1:-2,1:-2]), cmap="RdBu",interpolation='bicubic')
-        plt.imshow(np.rot90(self.p[:,:,16]), cmap="RdBu",interpolation='bicubic')
+        plt.imshow(np.rot90(self.U[:,:,5]), cmap="RdBu",interpolation='bicubic')
         plt.colorbar()
         plt.axis("equal")
 

3DSolvers/CompactSchemes.py
@@ -20,7 +20,8 @@ def cyclic2(a, b, bb, c, alpha, beta, r, n, u, x, z):
     bb[-1]  = b[-1]-alpha*beta/gamma
 
     x = sll.dgtsv(a,bb,c,r)[3]
-
+    
+    u[:] = 0.0
     u[0] = gamma
     u[-1] = alpha
 
@@ -435,42 +436,42 @@ class CollocatedDeriv:
             return sll.dgtsv(self.a2,self.b2,self.c2,spspar.csr_matrix.dot(self.RH2D,f))[3]
     
     def df_3D(self,f): 
-        
+        temp = np.zeros((self.Nx,self.Ny,self.Nz),dtype=np.double)
         if self.dir == "X":
             for j in range(self.Ny):
                 for k in range(self.Nz):
-                    self.temp[:,j,k] = self.compact1stDeriv_Fast(f[:,j,k])
-            return self.temp
+                    temp[:,j,k] = self.compact1stDeriv_Fast(f[:,j,k])
+            return temp
         elif self.dir == "Y":
             for i in range(self.Nx):
                 for k in range(self.Nz):
-                    self.temp[i,:,k] = self.compact1stDeriv_Fast(f[i,:,k])        
-            return self.temp      
+                    temp[i,:,k] = self.compact1stDeriv_Fast(f[i,:,k])        
+            return temp      
         elif self.dir == "Z":
             for i in range(self.Nx):
                 for j in range(self.Ny):
-                    self.temp[i,j,:] = self.compact1stDeriv_Fast(f[i,j,:])        
-            return self.temp           
+                    temp[i,j,:] = self.compact1stDeriv_Fast(f[i,j,:])        
+            return temp           
         else:
             print("Unknown direction")
 
     def d2f_3D(self,f): 
-        
+        temp = np.zeros((self.Nx,self.Ny,self.Nz),dtype=np.double)        
         if self.dir == "X":
             for j in range(self.Ny):
                 for k in range(self.Nz):
-                    self.temp[:,j,k] = self.compact2ndDeriv_Fast(f[:,j,k])
-            return self.temp
+                    temp[:,j,k] = self.compact2ndDeriv_Fast(f[:,j,k])
+            return temp
         elif self.dir == "Y":
             for i in range(self.Nx):
                 for k in range(self.Nz):
-                    self.temp[i,:,k] = self.compact2ndDeriv_Fast(f[i,:,k])        
-            return self.temp      
+                    temp[i,:,k] = self.compact2ndDeriv_Fast(f[i,:,k])        
+            return temp      
         elif self.dir == "Z":
             for i in range(self.Nx):
                 for j in range(self.Ny):
-                    self.temp[i,j,:] = self.compact2ndDeriv_Fast(f[i,j,:])        
-            return self.temp           
+                    temp[i,j,:] = self.compact2ndDeriv_Fast(f[i,j,:])        
+            return temp           
         else:
             print("Unknown direction")
 

3DSolvers/ObjectCollocatedSolver.py
@@ -16,9 +16,9 @@ from CollocatedSolver import CSolver3D
 plt.ion()
 
 #Domain information
-Nx = 32    
-Ny = 32
-Nz = 32
+Nx = 10   
+Ny = 10
+Nz = 10
 Lx = 1.0
 Ly = 1.0
 Lz = 1.0
@@ -29,29 +29,29 @@ domain = Domain3D(Nx, Ny, Nz, x, y, z, Lx, Ly, Lz)
 [X, Y, Z] = np.meshgrid(x, y, z)
 
 #Boundary Condition information
-bcXType = "DIRICHLET"
-bcX0 = "ADIABATIC_WALL"
-bcX1 = "ADIABATIC_WALL"
+bcXType = "PERIODIC"
+bcX0 = "PERIODIC"
+bcX1 = "PERIODIC"
 bcYType = "DIRICHLET"
 bcY0 = "ADIABATIC_WALL"
-bcY1 = "ADIABATIC_WALL"
-bcZType = "DIRICHLET"
-bcZ0 = "ADIABATIC_WALL"
-bcZ1 = "ADIABATIC_WALL"
+bcY1 = "ADIABATIC_MOVINGWALL"
+bcZType = "PERIODIC"
+bcZ0 = "PERIODIC"
+bcZ1 = "PERIODIC"
 bc = BC3D(bcXType, bcX0, bcX1, bcYType, bcY0, bcY1, bcZType, bcZ0, bcZ1)
 
 #Time stepping information
 CFL = 0.25
-maxTimeStep = 1000
+maxTimeStep = 100000
 maxTime = 1000
 plotStep = 1
-filterStep = 1
+filterStep = 10
 timestepping = TimeStepping(CFL, maxTimeStep, maxTime, plotStep, filterStep)
 
 #Filtering stuff
-alphaF = 0.45
+alphaF = 0.2
 #Fluid stuff
-mu_ref = 0.0000001
+mu_ref = 0.1
 
 ##Ghia results
 #yy = np.array([0.0, 0.0547, 0.0625, 0.0703, 0.1016, 0.1719, 0.2813, 0.4531, 0.50, 0.6172, 0.7344, 0.8516,  0.9531, 0.9609, 0.9688, 0.9766, 1.0])
@@ -74,9 +74,11 @@ p0   = np.ones((Nx,Ny,Nz))
 for i in range(Nx):
     for j in range(Ny):
         for k in range(Nz):
-#                U0[i,j,k] = 0.0
-#                V0[i,j,k] = 0.0
-#                W0[i,j,k] = 0.0
+                U0[i,j,k] = 0.0
+                V0[i,j,k] = 0.0
+                W0[i,j,k] = 0.0
+                rho0[i,j,k] = 1.0
+                p0[i,j,k] = 1/1.4
 #                if x[i] < 0.5:
 #                    rho0[i,j,k] = 1.0
 #                    p0[i,j,k] = 1.0/csolver.idealGas.gamma 
@@ -84,18 +86,18 @@ for i in range(Nx):
 #                    rho0[i,j,k] = 0.125
 #                    p0[i,j,k] = 0.1/csolver.idealGas.gamma
 #            
-            if x[i] > Lx/4 and x[i] < 3*Lx/4:
-                U0[i,j,k]   = 0.0
-                V0[i,j,k]   = 0.0
-                W0[i,j,k]   = 0.0
-                rho0[i,j,k] = 1 + 0.005*np.exp(-((x[i]-(Lx/2))**2 + (y[j]-(Ly/2))**2)/0.001)
-                p0[i,j,k]   = (1 + 0.005*np.exp(-((x[i]-(Lx/2))**2 + (y[j]-(Ly/2))**2)/0.001))/csolver.idealGas.gamma
-            else:
-                U0[i,j,k]   = 0.0
-                V0[i,j,k]   = 0.0
-                W0[i,j,k]   = 0.0
-                rho0[i,j,k] = 1.0
-                p0[i,j,k]   = 1.0/csolver.idealGas.gamma
+#            if x[i] > Lx/4 and x[i] < 3*Lx/4:
+#                U0[i,j,k]   = 0.0
+#                V0[i,j,k]   = 0.0
+#                W0[i,j,k]   = 0.0
+#                rho0[i,j,k] = 1 + 0.005*np.exp(-((x[i]-(Lx/2))**2 + (y[j]-(Ly/2))**2)/0.001)
+#                p0[i,j,k]   = (1 + 0.005*np.exp(-((x[i]-(Lx/2))**2 + (y[j]-(Ly/2))**2)/0.001))/csolver.idealGas.gamma
+#            else:
+#                U0[i,j,k]   = 0.0
+#                V0[i,j,k]   = 0.0
+#                W0[i,j,k]   = 0.0
+#                rho0[i,j,k] = 1.0
+#                p0[i,j,k]   = 1.0/csolver.idealGas.gamma
 
 #Set the initial conditions in the solver        
 csolver.setInitialConditions(rho0, U0, V0, W0, p0)